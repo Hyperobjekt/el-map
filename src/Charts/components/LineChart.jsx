516e651f88705c02bb619d27e16d7c8556b4d14a
Josh Maas-Howard <j.maas.howard@gmail.com>
2022-04-15 16:23:22 -0700

    nat avg prog

--- src/Charts/components/LineChart.jsx
+++ src/Charts/components/LineChart.jsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import useLineData from "../hooks/useLineData";
 import { useBubbleContext } from "@hyperobjekt/react-dashboard";
 import {
@@ -19,6 +19,7 @@ import { LinePath } from "@visx/shape";
 import { getColorForIndex } from "../../utils";
 import { defaultStyles } from "@visx/tooltip";
 import useConfidenceIntervalData from "../hooks/useConfidenceIntervalData";
+import useNationalAverageData from "../hooks/useNationalAverageData";
 
 const accessors = {
   xAccessor: (d) => d.x,
@@ -38,10 +39,20 @@ const LineChart = ({
   margin,
   ...props
 }) => {
+  const [natAvgActive, setNatAvgActive] = useState(false);
+  const [natAvgData, setNatAvgData] = useState([]);
+  
   const { metric_id } = useBubbleContext();
   // TODO: nat_avg_active, conf_int_active
   const lines = useLineData(metric_id);
   const confidenceIntervals = useConfidenceIntervalData(metric_id);
+
+  useEffect(async () => {
+    console.log("TOEH")
+    const data = await useNationalAverageData(metric_id);
+    console.log("NA: ", data)
+    setNatAvgData(data);
+  }, [])
 
   const xMax = width - margin.left - margin.right;
   const yMax = height - margin.top - margin.bottom;
@@ -56,122 +67,46 @@ const LineChart = ({
         // range: [62, 595]
         range: [0, xMax],
       },
-      [lines]
+      [locations, lines, metric_id]
     )
   );
-  const yScale = useMemo(() =>
-    scaleLinear(
+  const yScale = useMemo(() => {
+    const lMin = getExtremeFromLines(lines, Math.min, (d) => d.y);
+    const cMin = getExtremeFromLines(
+      confidenceIntervals,
+      Math.min,
+      (d) => d.y0
+    );
+    const min = Math.min(lMin, cMin);
+
+    const lMax = getExtremeFromLines(lines, Math.max, (d) => d.y);
+    const cMax = getExtremeFromLines(
+      confidenceIntervals,
+      Math.max,
+      (d) => d.y1
+    );
+    const max = Math.max(lMax, cMax);
+    console.log("change scale!", min, max, cMin, lMin, lMax, cMax);
+
+    return scaleLinear(
       {
-        domain: [
-          Math.min(
-            getExtremeFromLines(lines, Math.min, (d) => d.y),
-            getExtremeFromLines(confidenceIntervals, Math.min, (d) => d.y0)
-          ),
-          Math.max(
-            getExtremeFromLines(lines, Math.max, (d) => d.y),
-            getExtremeFromLines(confidenceIntervals, Math.max, (d) => d.y1)
-          ),
-        ],
+        domain: [min, max],
         // range: [340, 10]
         range: [yMax, 0],
       },
-      [lines]
-    )
-  );
+      [locations, lines, metric_id]
+    );
+  });
 
   return (
     <div className={clsx("line-chart__root", className)} {...props}>
-      {/* TODO: insert conf_int_chart */}
-      {/* <XYChart
-        height={400}
-        xScale={{ type: "band" }}
-        yScale={{ type: "linear" }}
-      >
-        <AnimatedAxis orientation="bottom" />
-        <AnimatedGrid columns={false} numTicks={4} />
-        {lines.map(({ GEOID, name, parent, data }, i) => {
-          return (
-                 <>
-          <Threshold
-            id={`${name}-threshold`}
-            data={data}
-            x={d => console.log(xScale(d.x)) ||  xScale(d.x)}
-            y0={(d) => console.log("y0", yScale(d.y * .9)) || yScale(d.y * .9)}
-            // y0={(d) => console.log("y0", d.y * 0.5) || d.y * 0.5}
-            y1={(d) => console.log("y1", yScale(d.y * 1.1)) || yScale(d.y * 1.1)}
-            // y1={(d) => console.log("y1", d.y * 1.5) || d.y * 1.5}
-            clipAboveTo={0}
-            clipBelowTo={400}
-            // curve={curveBasis}
-            belowAreaProps={{
-              fill: 'violet',
-              fillOpacity: 0.4,
-            }}
-            aboveAreaProps={{
-              fill: 'green',
-              fillOpacity: 0.4,
-            }}
-          />
-          <LinePath
-            data={data}
-            // curve={curveBasis}
-            x={d => xScale(d.x)}
-            y={(d) => yScale(d.y)}
-            stroke="#222"
-            strokeWidth={1.5}
-            strokeOpacity={0.8}
-            strokeDasharray="1,2"
-          />
-            <AnimatedLineSeries
-              key={GEOID}
-              dataKey={name}
-              data={data}
-              stroke={getColorForIndex(i)}
-              {...accessors}
-              />
-              </>
-          );
-        })}
-        <Tooltip
-          style={{ ...defaultStyles, zIndex: 1000 }}
-          showVerticalCrosshair
-          showSeriesGlyphs
-          verticalCrosshairStyle={{
-            stroke: "rgba(0, 0, 0, 0.1)",
-          }}
-          renderTooltip={(tooltipProps) => {
-            console.log("toop tip data", tooltipProps);
-            const { tooltipData, colorScale } = tooltipProps;
-            return (
-              <div>
-                <div
-                  style={{ color: colorScale(tooltipData.nearestDatum.key) }}
-                >
-                  {tooltipData.nearestDatum.key}
-                </div>
-                {accessors.xAccessor(tooltipData.nearestDatum.datum)}
-                {", "}
-                {accessors.yAccessor(tooltipData.nearestDatum.datum)}
-              </div>
-            );
-          }}
-        />
-      </XYChart> */}
       <svg height={height} width={width}>
         {/* <rect x={0} y={0} width={width} height={height} fill={background} rx={14} /> */}
         <Group left={margin.left} top={margin.top} right={margin.right}>
-          {/* <GridRows scale={temperatureScale} width={xMax} height={yMax} stroke="#e0e0e0" />
-          <GridColumns scale={timeScale} width={xMax} height={yMax} stroke="#e0e0e0" /> */}
-          {/* <line x1={xMax} x2={xMax} y1={0} y2={yMax} stroke="#e0e0e0" /> */}
-          {/* <AxisBottom top={yMax} scale={timeScale} numTicks={width > 520 ? 10 : 5} />
-          <AxisLeft scale={temperatureScale} /> */}
           <text x={-210} y={-40} transform="rotate(-90)" fontSize={10}>
             (The Metrix)
           </text>
-          {/* <AnimatedAxis orientation="bottom" />
-          <AnimatedGrid columns={false} numTicks={4} /> */}
           <AxisLeft scale={yScale} />
-          {/* <AxisBottom scale={xScale} /> */}
           <Axis top={yMax} scale={xScale} tickFormat={(d) => String(d)} />
           {confidenceIntervals.map(({ name, data }, i) => {
             return (
@@ -195,8 +130,8 @@ const LineChart = ({
                   stroke: "#f4f7f9",
                   strokeWidth: 0.1,
                   // stroke: "none"
-                  fill: getColorForIndex(i) + "11",
-                  // fill: "transparent"
+                  // fill: getColorForIndex(i) + "11",
+                  fill: "transparent"
                   // fillOpacity: 0.4,
                 }}
                 aboveAreaProps={{
@@ -204,7 +139,7 @@ const LineChart = ({
                   // stroke: "#f4f7f9",
                   // strokeWidth: 0.1,
                   // stroke: "none"
-                  fill: getColorForIndex(i) + "11",
+                  fill: getColorForIndex(i) + "20",
                   // fillOpacity: 0.4,
                 }}
               />
@@ -219,7 +154,7 @@ const LineChart = ({
                 x={(d) => xScale(d.x)}
                 y={(d) => yScale(d.y)}
                 stroke={getColorForIndex(i)}
-                strokeWidth={2}
+                strokeWidth={3}
                 strokeOpacity={1}
               />
             );
--- src/Charts/hooks/useNationalAverageData.js
+++ src/Charts/hooks/useNationalAverageData.js
@@ -1,31 +1,28 @@
 import { useFullLocationData } from "../../hooks";
 import { useAppConfig, useAccessor } from "@hyperobjekt/react-dashboard";
+import { csv } from "d3-fetch";
+import { useMemo } from "react";
+
+// const parseAvg = (avg = {}) => {};
 
 /**
  * Returns the confidence interval data for the selected locations
  * and national average (?).
  */
-export default function useNationalAverageData(metricId) {
-  const locationData = useFullLocationData();
-  const years = useAppConfig("years");
-  const accessor = useAccessor();
-  const locationLines = locationData.map((location) => {
-    const GEOID = location.GEOID;
-    const name = location.n;
-    const parent = location.pl;
-    const data = years.map((year) => {
-      // const key = accessor({ metric_id: metricId, year });
-      const keyL = accessor({ metric_id: metricId + "l", year });
-      const keyH = accessor({ metric_id: metricId + "h", year });
-      console.log(year, location[keyL], location[keyH]);
-      return {
-        x: year,
-        y0: location[keyL],
-        y1: location[keyH],
-      };
-    });
-    // TODO: omit all but data?
-    return { GEOID, name, parent, data };
-  });
-  return locationLines;
+export default async function useNationalAverageData(metricId) {
+  const avgUrl = useAppConfig("national_data");
+  const avgData = await useMemo(
+    async () => console.log("FETCH") || (await csv(avgUrl)),
+    []
+  );
+  // const avgData = await csv(avgUrl);
+
+  console.log("AA", avgUrl);
+  console.log("AA", avgData);
+  const metricKey = useAppConfig("metric_abbrev_map");
+  console.log(metricKey);
+  return avgData.map((d) => ({
+    x: d.year,
+    y: d[metricKey],
+  }));
 }

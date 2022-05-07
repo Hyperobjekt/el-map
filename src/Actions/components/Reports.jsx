80130e460246592ff3b3d5b78cd0f26094fda434
Josh Maas-Howard <j.maas.howard@gmail.com>
2022-05-06 17:10:36 -0700

    reports: - set up modal - mimic file-export demo request

--- src/Actions/components/Reports.jsx
+++ src/Actions/components/Reports.jsx
@@ -1,28 +1,34 @@
 import { Typography, Button, Box } from "@mui/material";
 import clsx from "clsx";
-import React from "react";
+import React, { useState } from "react";
 import { useLang } from "@hyperobjekt/react-dashboard";
+import ReportsModal from "./ReportsModal";
 
 const Reports = ({ className, ...props }) => {
+  const [modalOpen, setModalOpen] = useState(true);
+
   const [heading, description, buttonLabel] = useLang([
     "REPORTS_HEADING",
     "REPORTS_DESCRIPTION",
     "REPORTS_BUTTON_LABEL",
   ]);
-  const handleSelectReports = () => {};
+  const handleSelectReports = () => setModalOpen(true);
   return (
-    <Box className={clsx(className)} {...props}>
-      <Typography variant="h2">{heading}</Typography>
-      <Box className="actions__image" height={72}>
-        <img src="/assets/img/download.svg" alt="" />
+    <>
+      <ReportsModal open={modalOpen} setOpen={setModalOpen} />
+      <Box className={clsx(className)} {...props}>
+        <Typography variant="h2">{heading}</Typography>
+        <Box className="actions__image" height={72}>
+          <img src="/assets/img/download.svg" alt="" />
+        </Box>
+        <Typography variant="body2" color="textSecondary">
+          {description}
+        </Typography>
+        <Button variant="bordered" onClick={handleSelectReports}>
+          {buttonLabel}
+        </Button>
       </Box>
-      <Typography variant="body2" color="textSecondary">
-        {description}
-      </Typography>
-      <Button variant="bordered" onClick={handleSelectReports}>
-        {buttonLabel}
-      </Button>
-    </Box>
+    </>
   );
 };
 

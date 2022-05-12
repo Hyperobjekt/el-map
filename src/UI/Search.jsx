cce2aa0480788bce3db3f5647334fb14e839e615
Josh Maas-Howard <j.maas.howard@gmail.com>
2022-05-11 19:23:52 -0700

    integrate county data into search

--- src/UI/Search.jsx
+++ src/UI/Search.jsx
@@ -1,15 +1,57 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { csv } from "d3-fetch";
 import { styled } from "@mui/material/styles";
+import * as JsSearch from "js-search";
 import { Search as SearchIcon } from "../Icons";
-import {
-  debounce,
-  IconButton,
-  InputAdornment,
-  TextField,
-} from "@mui/material";
+import { useAppConfig } from "@hyperobjekt/react-dashboard";
+import { debounce, IconButton, InputAdornment, TextField } from "@mui/material";
 import { Close } from "@mui/icons-material";
 import AutocompleteStyle from "./Search.style";
 import clsx from "clsx";
+// import useSearchData from "./useSearchData";
+
+const geoTypes = "region,place,address";
+// state, county, city, address
+const geoPriority = ["region", "county", "place", "address"];
+const resultCount = 5;
+
+const parseCounty = ({ GEOID, name, north, south, east, west, lon, lat }) => ({
+  id: GEOID,
+  place_name: name,
+  bbox: [Number(west), Number(south), Number(east), Number(north)],
+  center: [Number(lon), Number(lat)],
+});
+
+const getTopResults = ({ geocodeResults = [], countyResults = [] }) => {
+  const results = [];
+  let geoIdx = 0;
+  while (results.length <= resultCount && geoIdx < geoPriority.length) {
+    const emptySlots = resultCount - results.length;
+    const geoType = geoPriority[geoIdx];
+    console.log({ results, geoType });
+    if (geoType === "county") {
+      const topCounties = countyResults.slice(0, emptySlots);
+      results.push(...topCounties);
+    } else {
+      const resultsOfGeotype = geocodeResults.filter(({ place_type }) =>
+        place_type.includes(geoType)
+      );
+      const topResults = resultsOfGeotype.slice(0, emptySlots);
+      results.push(...topResults);
+    }
+
+    geoIdx++;
+  }
+  console.log({ results, geoIdx });
+  return results;
+};
+
+// TODO: extract mbt
+// could use proximity=ip or proximity=${viewport.longitude},${viewport.latitude}
+const getPath = (inputValue) =>
+  `https://api.mapbox.com/geocoding/v5/mapbox.places/${inputValue}.json?access_token=${"pk.eyJ1IjoiaHlwZXJvYmpla3QiLCJhIjoiY2pzZ3Bnd3piMGV6YTQzbjVqa3Z3dHQxZyJ9.rHobqsY_BjkNbqNQS4DNYw"}&cachebuster=${Math.floor(
+    Date.now()
+  )}&types=${geoTypes}&autocomplete=true&country=US`;
 
 const StyledSearchInput = styled("div")(({ theme }) => ({
   position: "relative",
@@ -46,53 +88,6 @@ const StyledInput = styled(TextField)(({ theme }) => ({
   },
 }));
 
-// const SearchInput = ({
-//   onChange,
-//   handleClear,
-//   value,
-//   inputParams = {},
-//   placeholder = "Search…",
-//   icon = <SearchIcon />,
-//   ...props
-// }) => {
-//   // const inputEl = useRef();
-//   // const handleClear = (event) => {
-//   //   inputEl.current.value = "";
-//   //   onClear && onClear(event);
-//   //   inputEl.current?.focus();
-//   // };
-//   // const { InputLabelProps, InputProps, ...rest } = inputParams;
-//   return (
-//     // <StyledSearchInput {...props}>
-//     <StyledSearchInput>
-//       <SearchIconWrapper>{icon}</SearchIconWrapper>
-//       <StyledInput
-//         variant="outlined"
-//         placeholder={placeholder}
-//         value={value}
-//         inputProps={{ "aria-label": "search", ...inputParams }}
-//         // InputLabelProps={InputLabelProps}
-//         InputProps={{
-//           // inputRef: inputEl,
-//           // ...inputParams,
-//           // ...InputLabelProps,
-//           // ...InputProps,
-//           // ...rest,
-//           endAdornment: !value.length ? null : (
-//             <InputAdornment position="end">
-//               <IconButton size="small" sx={{ mr: 1 }} onClick={handleClear}>
-//                 <Close style={{ fontSize: 16 }} />
-//               </IconButton>
-//             </InputAdornment>
-//           ),
-//         }}
-//         onChange={onChange}
-//         // {...inputParams}
-//       />
-//     </StyledSearchInput>
-//   );
-// };
-
 const Search = ({
   placeholder = "Search...",
   icon = <SearchIcon />,
@@ -102,30 +97,40 @@ const Search = ({
   const onChange = (e) => setInputValue(e.target.value);
   const handleClear = () => setInputValue("");
   const [results, setResults] = useState([]);
+  // const [counties, setCounties] = useState([]);
+  const [countySearch, setCountySearch] = useState(null);
+
+  const countiesUrl = useAppConfig("counties_data");
+  useEffect(() => {
+    csv(countiesUrl, parseCounty).then((counties) => {
+      // setCounties(d)
+
+      const searchFn = new JsSearch.Search("id");
+      searchFn.addIndex("place_name");
+      searchFn.addDocuments(counties);
+      setCountySearch(searchFn);
+    });
+  }, []);
+  // console.log({counties })
 
   const validSearchTerm = inputValue.length >= 3;
   const executeSearch = () => {
     // const inputValue = e.target.value
     console.log({ inputValue });
     if (!validSearchTerm) return setResults([]);
-    const path = `https://api.mapbox.com/geocoding/v5/mapbox.places/${inputValue}.json?access_token=${"pk.eyJ1IjoiaHlwZXJvYmpla3QiLCJhIjoiY2pzZ3Bnd3piMGV6YTQzbjVqa3Z3dHQxZyJ9.rHobqsY_BjkNbqNQS4DNYw"}&cachebuster=${Math.floor(
-      Date.now()
-    )}&types=region,place,address&autocomplete=true&country=US${
-      // `&proximity=ip`
-      ""
-      // viewport.zoom > FULL_FUNCT_ZOOM_THRESHOLD
-      //   ? `&proximity=${viewport.longitude},${viewport.latitude}`
-      //   : ``
-    }`;
-    // Get request for autosuggest results.
+    const path = getPath(inputValue);
     fetch(path)
       .then((r) => r.json())
       .then((json) => {
-        setResults(json.features);
-        console.log({ json });
+        // console.log("RESPONSE")
+        const geocodeResults = json.features;
+        const countyResults =
+          (countySearch && countySearch.search(inputValue)) || [];
+        // console.log({ geocodeResults, countyResults });
+        setResults(getTopResults({ geocodeResults, countyResults }));
       });
-  }
-  const executeSearchDeb = debounce(executeSearch, 200)
+  };
+  const executeSearchDeb = debounce(executeSearch, 100);
   useEffect(executeSearchDeb, [inputValue]);
 
   const onSelect = (e) => {
@@ -142,7 +147,7 @@ const Search = ({
     <AutocompleteStyle
       className={clsx(className, "search__root")}
       forcePopupIcon={false}
-      value={inputValue ? { place_name: inputValue} : null}
+      value={inputValue ? { place_name: inputValue } : null}
       id="search-autocomplete"
       options={results}
       onSelect={onSelect}
@@ -169,7 +174,7 @@ const Search = ({
               InputLabelProps={InputLabelProps}
               InputProps={{
                 ...restInputProps,
-                endAdornment:  (
+                endAdornment: !!inputValue.length && (
                   <InputAdornment position="end">
                     <IconButton
                       size="small"

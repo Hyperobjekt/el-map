cca1a791c53dd2b275f0e6a1cf68158cc2eb6a34
Josh Maas-Howard <j.maas.howard@gmail.com>
2022-05-31 12:48:34 -0700

    fix: revert to previous useOnRouteLoad - keeps route up to date on location change - loads features from url params - doesn't seem to trigger rerenders(?)

--- src/Router/useOnRouteLoad.js
+++ src/Router/useOnRouteLoad.js
@@ -15,6 +15,50 @@ import useDataMode from "../hooks/useDataMode";
  * 1. takes the location string, pulls out the GEOID, longitude, and latitude
  * 2. fetches the GeoJSON features for the locations from the tileset
  * 3. sets the selected features in the location store
+ * 
+ * TODO: figure out why this reversion to 67fb7f90f73034495ce098e9689b61c00e34c701
+ * is undesireable/triggers app rerenders (as described below)
+ * @returns {function} ({config, defaultValues}) => Promise
+ */
+function useOnRouteLoad() {
+  const onLoad = useOnConfigLoad();
+  const [dataMode] = useDataMode();
+  const setLocationState = useLocationStore((state) => state.set);
+  return useCallback(
+    ({ config, defaultValues }) => {
+      console.log({ defaultValues });
+      return onLoad({ config, defaultValues }).then(
+        ({ config, defaultValues }) => {
+          const { locations } = defaultValues;
+          console.log({ locations });
+          if (!locations) return Promise.resolve({ config, defaultValues });
+          // use the string values to fetch the tile data
+          const locationPromises = locations.split("~").map((l) => {
+            const [geoid, lng, lat] = l.split("_");
+            return getTileData({ geoid, lngLat: { lng, lat }, dataMode });
+          });
+          // once all the feature have been retrieved, add them to the location store
+          return Promise.all(locationPromises).then((features) => {
+            if (Array.isArray(features))
+              setLocationState({ selected: features });
+            return { config, defaultValues, features };
+          });
+        }
+      );
+    },
+    [onLoad]
+  );
+}
+
+/**
+ * This hook provides a callback function that is used to set the application
+ * defaults and fetch any related data.  Most of the work is handled by
+ * @hyperobjet/react-dashboard's useOnConfigLoad callback.
+ *
+ * This hook additionally:
+ * 1. takes the location string, pulls out the GEOID, longitude, and latitude
+ * 2. fetches the GeoJSON features for the locations from the tileset
+ * 3. sets the selected features in the location store
  *
  * TODO: Load locations without triggering root level re-render
  *   The current loading code has been commented out because
@@ -25,37 +69,37 @@ import useDataMode from "../hooks/useDataMode";
  *
  * @returns {function} ({config, defaultValues}) => Promise
  */
-function useOnRouteLoad() {
-  const onLoad = useOnConfigLoad();
-  // const [dataMode] = useDataMode();
-  // const setLocationState = useLocationStore((state) => state.set);
-  // const selectedLocations = useLocationStore((state) => state.selected?.length);
-  return useCallback(
-    ({ config, defaultValues }) => {
-      return onLoad({ config, defaultValues });
-      // .then(
-      //   ({ config, defaultValues }) => {
-      //     const { locations } = defaultValues;
-      //     const locationStrings = locations?.split("~");
-      //     // resolve if no locations, or if locations have already been added
-      //     if (!locationStrings || locationStrings.length === selectedLocations)
-      //       return Promise.resolve({ config, defaultValues });
-      //     // use the string values to fetch the tile data
-      //     const locationPromises = locationStrings.map((l) => {
-      //       const [geoid, lng, lat] = l.split("_");
-      //       return getTileData({ geoid, lngLat: { lng, lat }, dataMode });
-      //     });
-      //     // once all the feature have been retrieved, add them to the location store
-      //     return Promise.all(locationPromises).then((features) => {
-      //       if (Array.isArray(features))
-      //         setLocationState({ selected: features });
-      //       return { config, defaultValues, features };
-      //     });
-      //   }
-      // );
-    },
-    [onLoad]
-  );
-}
+// function useOnRouteLoad() {
+//   const onLoad = useOnConfigLoad();
+//   // const [dataMode] = useDataMode();
+//   // const setLocationState = useLocationStore((state) => state.set);
+//   // const selectedLocations = useLocationStore((state) => state.selected?.length);
+//   return useCallback(
+//     ({ config, defaultValues }) => {
+//       return onLoad({ config, defaultValues });
+//       // .then(
+//       //   ({ config, defaultValues }) => {
+//       //     const { locations } = defaultValues;
+//       //     const locationStrings = locations?.split("~");
+//       //     // resolve if no locations, or if locations have already been added
+//       //     if (!locationStrings || locationStrings.length === selectedLocations)
+//       //       return Promise.resolve({ config, defaultValues });
+//       //     // use the string values to fetch the tile data
+//       //     const locationPromises = locationStrings.map((l) => {
+//       //       const [geoid, lng, lat] = l.split("_");
+//       //       return getTileData({ geoid, lngLat: { lng, lat }, dataMode });
+//       //     });
+//       //     // once all the feature have been retrieved, add them to the location store
+//       //     return Promise.all(locationPromises).then((features) => {
+//       //       if (Array.isArray(features))
+//       //         setLocationState({ selected: features });
+//       //       return { config, defaultValues, features };
+//       //     });
+//       //   }
+//       // );
+//     },
+//     [onLoad]
+//   );
+// }
 
 export default useOnRouteLoad;

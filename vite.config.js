7ffc2f9269f197a7aa83acc8973ed9ccc8045ecb
James Minton <james-minton@github.com>
2022-06-02 09:22:06 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/v2map/",
   build: {
     target: "esnext",
-    base: "/v2map/",
     // TODO: revert
     minify: false, // use for debugging
   },

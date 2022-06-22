57b0ac0c8f80874175ac5f68b4dc2f7c0059e840
Josh Maas-Howard <j.maas.howard@gmail.com>
2022-06-21 19:00:30 -0700

    feat: reports starting point

--- src/Actions/components/Reports.jsx
+++ src/Actions/components/Reports.jsx
@@ -3,17 +3,21 @@ import clsx from 'clsx';
 import React from 'react';
 import { useLang } from '@hyperobjekt/react-dashboard';
 import { getAssetPath } from '../../utils';
+import ReportsModal from './ReportsModal';
 
 const Reports = ({ className, ...props }) => {
+  const [modalOpen, setModalOpen] = useState(true);
+
   const [heading, description, buttonLabel] = useLang([
     'REPORTS_HEADING',
     'REPORTS_DESCRIPTION',
     'REPORTS_BUTTON_LABEL_TEMP',
     // "REPORTS_BUTTON_LABEL",
   ]);
-  const handleSelectReports = () => {};
+  const handleSelectReports = () => setModalOpen(true);
   return (
     <Box className={clsx(className)} {...props}>
+      <ReportsModal open={modalOpen} setOpen={setModalOpen} />
       <Typography variant="h2">{heading}</Typography>
       <Box className="actions__image" height={72}>
         <img src={getAssetPath('assets/img/download.svg')} alt="" />
@@ -21,7 +25,7 @@ const Reports = ({ className, ...props }) => {
       <Typography variant="body2" color="textSecondary">
         {description}
       </Typography>
-      <Button disabled={true} variant="bordered" onClick={handleSelectReports}>
+      <Button variant="bordered" onClick={handleSelectReports}>
         {buttonLabel}
       </Button>
     </Box>

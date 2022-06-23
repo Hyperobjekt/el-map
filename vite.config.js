65d2f69b16869c2d7431ed542413e00a7da0128c
Josh Maas-Howard <j.maas.howard@gmail.com>
2022-06-22 19:01:42 -0700

    chore: update lock files for deploy action

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { ENVIRONMENT } from './src/utils';
 
+// mode is 'production' for build, 'development' for serve
 const isProd = (mode) => mode === 'production' && ENVIRONMENT.context === 'prod';
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
   plugins: [react()],
-  // deployed static build  (from prod branch) gets different base path
+  // deployed static build (from prod branch) gets different base path
   base: isProd(mode) ? '/v2map/' : '/',
   build: {
     target: 'esnext',
